4161b71ca31cdc9dcb396e35f7a84aea46b24e96
davidski <davidski@deadheaven.com>
2014-05-23 13:01:46 -0700

    Add crontab for curator

---
+++ attrbutes/attributes.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: es-currator
+# Attributes:: default
+#
+
+default['elasticsearch-currator']['days_to_keep'] = '30'
+default['elasticsearch-currator']['elasticsearch-server'] = 'localhost'
