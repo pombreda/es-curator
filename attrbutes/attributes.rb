7f91d71e9ded18897e507f343ccd897eef2f160c
davidski <davidski@deadheaven.com>
2014-05-23 15:54:45 -0700

    Spelling is hard

--- attrbutes/attributes.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: es-currator
-# Attributes:: default
-#
-
-default['elasticsearch-currator']['days_to_keep'] = '30'
-default['elasticsearch-currator']['elasticsearch-server'] = 'localhost'
