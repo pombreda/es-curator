ada02ff7573c8a7e81527a92bd2389ec502ba671
davidski <davidski@deadheaven.com>
2014-06-20 11:52:52 -0700

    typo on nil attributes

--- attributes/attributes.rb
+++ attributes/attributes.rb
@@ -3,8 +3,8 @@
 # Attributes:: default
 #
 
-default['elasticsearch-curator']['days_to_keep'] =            nill
+default['elasticsearch-curator']['days_to_keep'] =            nil
 default['elasticsearch-curator']['elasticsearch-server'] =    'localhost'
 default['elasticsearch-curator']['hour_to_run'] =             '9'
-default['elasticsearch-curator']['optimize_indicies_after'] = nill
-default['elasticsearch-curator']['snapshot_repository'] =     nill
+default['elasticsearch-curator']['optimize_indicies_after'] = nil
+default['elasticsearch-curator']['snapshot_repository'] =     nil
