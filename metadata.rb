284f3c0f32ca0daad4abe9644763ea16e6e14e39
davidski <davidski@deadheaven.com>
2014-07-13 07:04:00 -0700

    Update curator python dependecies

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures es-curator'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'python', '>= 1.4.7'
-depends 'cron', '>= 1.4.1'
+depends 'python', '~> 1.4.6'
+depends 'cron', '~> 1.4.1'
