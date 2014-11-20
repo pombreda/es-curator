00fa83cf771c2a63b354b493416ae07d2db4fbf1
davidski <davidski@deadheaven.com>
2014-11-20 13:16:21 -0800

    Separate out snapshot creation from pruning steps

--- recipes/default.rb
+++ recipes/default.rb
@@ -54,5 +54,14 @@ cron_d 'es-curator-backup' do
   weekday #{node['elasticsearch-curator']['backup_weekday']}
   minute  0
   hour    #{node['elasticsearch-curator']['hour_to_run']}
-  command "/usr/local/bin/curator --timeout #{node['elasticsearch-curator']['timeout']} --host #{node['elasticsearch-curator']['elasticsearch_server']} snapshot --repository #{node['elasticsearch-curator']['snapshot_repository']} --older-than #{node['elasticsearch-curator']['backup_indicies_older_than']} --delete-older-than #{node['elasticsearch-curator']['delete_snapshots_older_than']}"
+  command "/usr/local/bin/curator --timeout #{node['elasticsearch-curator']['timeout']} --host #{node['elasticsearch-curator']['elasticsearch_server']} snapshot --repository #{node['elasticsearch-curator']['snapshot_repository']} --older-than #{node['elasticsearch-curator']['backup_indicies_older_than']}"
+end
+
+# schedule pruning backups
+cron_d 'es-curator-snapshot-prune' do
+  action node['elasticsearch-curator'].attribute?(:snapshot_repository) ? :create : :delete
+  weekday #{node['elasticsearch-curator']['backup_weekday']}
+  minute  0
+  hour    #{node['elasticsearch-curator']['hour_to_run']}
+  command "/usr/local/bin/curator --timeout #{node['elasticsearch-curator']['timeout']} --host #{node['elasticsearch-curator']['elasticsearch_server']} snapshot --repository #{node['elasticsearch-curator']['snapshot_repository']} --delete-older-than #{node['elasticsearch-curator']['delete_snapshots_older_than']}"
 end
